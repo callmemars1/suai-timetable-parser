ffa6d218c8913c0015d209c72e10cc543a1e46ab
LomasovEgor <egorka.lomasov@gmail.com>
2022-04-30 02:39:54 +0300

    create take_rasp_body.py

--- take_rasp_body.py
+++ take_rasp_body.py
@@ -19,20 +19,20 @@ lesson_number = class_result.find_all('h4')
 class_study = class_result.find_all('div', class_='study')
 chota = class_result.find('h3').find_next_sibling('h4').find_next_sibling().find_next()
 
-
-h3_tags = class_result.find_all('h3')
-for index, h3 in enumerate(h3_tags):
-    print("---------------------------------------------------------")
-    print(h3_tags[index])
-    while h3.find_next_sibling() != h3_tags[index + 1]:
-        nex_el = h3.find_next_sibling()
-        h3 = nex_el
-        print(nex_el)
-
-        ###
-
-
-# print(day)
-# print(lesson_number)
-# print(class_study)
-# print(chota)
+#
+# h3_tags = class_result.find_all('h3')
+# for index, h3 in enumerate(h3_tags):
+#     print("---------------------------------------------------------")
+#     print(h3_tags[index])
+#     while h3.find_next_sibling() != h3_tags[index + 1]:
+#         nex_el = h3.find_next_sibling()
+#         h3 = nex_el
+#         print(nex_el)
+#
+#         ###
+#
+#
+# # print(day)
+# # print(lesson_number)
+# # print(class_study)
+# # print(chota)
