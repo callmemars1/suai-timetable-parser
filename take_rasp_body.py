e2cfd5e96ffe2178b7b2dfff94f7203a08dfe3be
LomasovEgor <egorka.lomasov@gmail.com>
2022-05-03 19:57:19 +0300

    main loop completed

--- take_rasp_body.py
+++ take_rasp_body.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup as bs
 import requests
+import logging
 import json
 import re
 
@@ -11,7 +12,6 @@ REQ_URL = 'https://rasp.guap.ru/?g=315'
 req = requests.get(REQ_URL)
 soup = bs(req.text, "html.parser")
 
-# combobox_info = soup.find('div', class_='form').find('select').find_all('option')
 class_result = soup.find('div', class_='result')
 
 day = class_result.find_all('h3')
@@ -20,17 +20,20 @@ class_study = class_result.find_all('div', class_='study')
 chota = class_result.find('h3').find_next_sibling('h4').find_next_sibling().find_next()
 
 
-# h3_tags = class_result.find_all('h3')
-# for index, h3 in enumerate(h3_tags):
-#     print("---------------------------------------------------------")
-#     print(h3_tags[index])
-#     while h3.find_next_sibling() != h3_tags[index + 1]:
-#         nex_el = h3.find_next_sibling()
-#         h3 = nex_el
-#         print(nex_el)
-#
-#         ###
-
+h3_tags = class_result.find_all('h3')
+for index, h3 in enumerate(h3_tags):
+    print("---------------------------------------------------------")
+    print(f'{h3_tags[index]=}')
+    if index + 1 < len(h3_tags):
+        while h3.find_next_sibling() != h3_tags[index + 1]:
+            nex_el = h3.find_next_sibling()
+            h3 = nex_el
+            print(nex_el)
+    else:
+        while h3.find_next_sibling() is not None:
+            nex_el = h3.find_next_sibling()
+            h3 = nex_el
+            print(nex_el)
 
 # print(day)
 # print(lesson_number)
