c0eae781f0f99fe821bec1b960de727cce1b382f
LomasovEgor <egorka.lomasov@gmail.com>
2022-05-03 21:44:08 +0300

    create RaspBodyParser class

--- take_rasp_body.py
+++ take_rasp_body.py
@@ -4,40 +4,45 @@ import logging
 import json
 import re
 
-group_table = []
-TABLE_PATH = 'table.json'
+TEST_URL = 'https://rasp.guap.ru/?g=315'
 
-REQ_URL = 'https://rasp.guap.ru/?g=315'
 
-req = requests.get(REQ_URL)
-soup = bs(req.text, "html.parser")
+class RaspBodyParser:
+    def __init__(self, url):
+        self.url = url
+        self.request = self.get_request()
+        self.soup = bs(self.request.text, "html.parser")
+        self.class_result = self.soup.find('div', class_='result')
 
-class_result = soup.find('div', class_='result')
+    def get_request(self):
+        try:
+            request = requests.get(self.url)
+            if request.status_code == 200:
+                return request
+        except:
+            print("Connection error")
 
-day = class_result.find_all('h3')
-lesson_number = class_result.find_all('h4')
-class_study = class_result.find_all('div', class_='study')
-chota = class_result.find('h3').find_next_sibling('h4').find_next_sibling().find_next()
+    def divide_by_days(self):
+        days = []
 
-days = []
+        h3_tags = self.class_result.find_all('h3')
+        for index, h3 in enumerate(h3_tags):
+            day = [h3_tags[index]]
+            if index + 1 < len(h3_tags):
+                while h3.find_next_sibling() != h3_tags[index + 1]:
+                    nex_el = h3.find_next_sibling()
+                    h3 = nex_el
+                    day.append(nex_el)
+            else:
+                while h3.find_next_sibling() is not None:
+                    nex_el = h3.find_next_sibling()
+                    h3 = nex_el
+                    day.append(nex_el)
+            days.append(day)
+        return days
 
-h3_tags = class_result.find_all('h3')
-for index, h3 in enumerate(h3_tags):
-    day = []
-    day.append(h3_tags[index])
-    if index + 1 < len(h3_tags):
-        while h3.find_next_sibling() != h3_tags[index + 1]:
-            nex_el = h3.find_next_sibling()
-            h3 = nex_el
-            day.append(nex_el)
-            # print(nex_el)
-    else:
-        while h3.find_next_sibling() is not None:
-            nex_el = h3.find_next_sibling()
-            h3 = nex_el
-            # print(nex_el)
-            day.append(nex_el)
-    days.append(day)
 
-for day in days:
+rasp_5038 = RaspBodyParser(TEST_URL)
+
+for day in rasp_5038.divide_by_days():
     print(day)
