17b3e9ad519c03527172e2a5de379612075e8d49
LomasovEgor <egorka.lomasov@gmail.com>
2022-06-03 00:50:55 +0300

    added saving into mangoDB

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 import json
 from take_rasp_body import RaspBodyParser
-
+from take_params import RaspParamParser
+from upload_manager import UploadManager
+from datetime import datetime
+from config import DB_USER, DB_NAME, DB_HOST, DB_PASS
 
 TABLE_PATH = 'full_result.json'
-g_params_path = 'g_param_table.json'
 
-with open(g_params_path, encoding='utf8') as file:
-    g_params: list = json.load(file)
 
-answer = []
+def main():
 
-print(len(g_params))
-for group in g_params:
-    g = group['value']
-    ref = f'https://rasp.guap.ru/?g={g}'
-    print(g)
-    rasp = RaspBodyParser(ref)
-    answer.append(rasp.crt_dict())
+    start_time = datetime.now()
 
-with open(TABLE_PATH, 'w', encoding='utf8') as file:
-    json.dump(answer, file, indent=2, ensure_ascii=False)
+    params_parser = RaspParamParser('https://rasp.guap.ru/')
+    g_params = params_parser.g_param_table
+
+    lessons = []
+    for group in g_params:
+        g = group['value']
+        ref = f'https://rasp.guap.ru/?g={g}'
+        rasp = RaspBodyParser(ref)
+        for lesson in rasp.crt_dict():
+            if lesson not in lessons:
+                lessons.append(lesson)
+
+    print(datetime.now() - start_time)
+
+    with open(TABLE_PATH, 'w', encoding='utf8') as file:
+        json.dump(lessons, file, indent=2, ensure_ascii=False)
+
+    UploadManager.upload_to_the_mangodb(DB_USER, DB_PASS, DB_HOST, DB_NAME, 'lessons', lessons)
+
+
+if __name__ == '__main__':
+    main()
